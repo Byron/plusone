a3dd0b02ef6026d832b14dc9b569849f2aba655e
Sebastian Thiel <sthiel@thoughtworks.com>
2015-09-26 14:42:38 +0200

    greatly simplified Rust program
    never put loops into the function unless you really have to

--- plusone-rs/src/bin/p1.rs
+++ plusone-rs/src/bin/p1.rs
@@ -5,13 +5,16 @@ use std::process;
 use std::io::{self, Write};
 
 fn main() {
-    const SKIP_COUNT: usize = 1;
-    if plusone::increment_strings(env::args().skip(SKIP_COUNT), |res| {
-            match res {
-                Ok(val)        => { writeln!(io::stdout(), "{}", val).unwrap(); },
-                Err((arg, _ )) => { writeln!(io::stderr(), "'{}' is not a number", arg).ok(); },
-            };
-        }) != env::args_os().count() - SKIP_COUNT {
-        process::exit(2);
+    let mut status = 0;
+    for arg in env::args().skip(1) {
+        match plusone::increment_string(&arg) {
+            Ok(val) => { writeln!(io::stdout(), "{}", val).unwrap(); },
+            Err(_)  => {
+                status = 2;
+                writeln!(io::stderr(), "'{}' is not a number", arg).ok();
+            },
+        };
     }
+
+    process::exit(status);
 }
--- plusone-rs/src/lib.rs
+++ plusone-rs/src/lib.rs
@@ -1,28 +1,13 @@
 use std::num::ParseIntError;
 
-pub fn increment_strings<I, T, F>(input: I, mut cb: F)
-    -> usize
-    where I: Iterator<Item=T>,
-          T: AsRef<str>,
-          F: FnMut(Result<usize, (&str, ParseIntError)>) {
-    let mut valid: usize = 0;
-    for item in input {
-        cb(item.as_ref().parse::<usize>()
-            .map(|v| {
-                valid += 1;
-                v+1
-            })
-            .map_err(|err| (item.as_ref(), err)));
-    }
-    valid
+pub fn increment_string<'a, T: AsRef<str> + 'a>(input: T) -> Result<isize, ParseIntError> {
+    input.as_ref().parse::<isize>()
+        .map(|v| v+1)
 }
 
 #[test]
 fn test_name() {
-    let mut count = 0;
-    assert_eq!(increment_strings(["5", "bar"].iter(), |res| count += res.is_ok() as usize), 1);
-    assert_eq!(count, 1);
-    let mut err_count = 0;
-    assert_eq!(increment_strings(["5", "6"].iter(), |res| err_count += res.is_err() as usize), 2);
-    assert_eq!(err_count, 0);
+    assert_eq!(increment_string("5"), Ok(6));
+    assert_eq!(increment_string("-1"), Ok(0));
+    assert!(increment_string("bar").is_err());
 }
