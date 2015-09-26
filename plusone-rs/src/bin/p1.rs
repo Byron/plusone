cbe05c216d93c9028cb165bd1a71b874c7cf41f8
Sebastian Thiel <sthiel@thoughtworks.com>
2015-09-26 11:59:05 +0200

    much more flexibility at the cost of higher complexity

--- plusone-rs/src/bin/p1.rs
+++ plusone-rs/src/bin/p1.rs
@@ -2,10 +2,16 @@ extern crate plusone;
 
 use std::env;
 use std::process;
+use std::io::{self, Write};
 
 fn main() {
     const SKIP_COUNT: usize = 1;
-    if plusone::increment_and_print_args(env::args().skip(SKIP_COUNT)) != env::args_os().count() - SKIP_COUNT {
+    if plusone::increment_strings(env::args().skip(SKIP_COUNT), |res| {
+            match res {
+                Ok(val)        => { writeln!(io::stdout(), "{}", val).unwrap(); },
+                Err((arg, _ )) => { writeln!(io::stderr(), "'{}' is not a number", arg).ok(); },
+            };
+        }) != env::args_os().count() - SKIP_COUNT {
         process::exit(2);
     }
 }
--- plusone-rs/src/lib.rs
+++ plusone-rs/src/lib.rs
@@ -1,26 +1,28 @@
-use std::io::{self, Write};
+use std::num::ParseIntError;
 
-pub fn increment_and_print_args<I, T>(input: I)
+pub fn increment_strings<I, T, F>(input: I, mut cb: F)
     -> usize
     where I: Iterator<Item=T>,
-          T: AsRef<str> {
+          T: AsRef<str>,
+          F: FnMut(Result<usize, (&str, ParseIntError)>) {
     let mut valid: usize = 0;
     for item in input {
-        let incremented_val = match item.as_ref().parse::<usize>() {
-            Ok(val) => val + 1,
-            Err(_) => {
-                writeln!(io::stderr(), "'{}' is not a number", item.as_ref()).ok();
-                continue
-            }
-        };
-        valid += 1;
-        writeln!(io::stdout(), "{}", incremented_val).unwrap();
+        cb(item.as_ref().parse::<usize>()
+            .map(|v| {
+                valid += 1;
+                v+1
+            })
+            .map_err(|err| (item.as_ref(), err)));
     }
     valid
 }
 
 #[test]
 fn test_name() {
-    assert_eq!(increment_and_print_args(["5", "bar"].iter()), 1);
-    assert_eq!(increment_and_print_args(["5", "6"].iter()), 2);
+    let mut count = 0;
+    assert_eq!(increment_strings(["5", "bar"].iter(), |res| count += res.is_ok() as usize), 1);
+    assert_eq!(count, 1);
+    let mut err_count = 0;
+    assert_eq!(increment_strings(["5", "6"].iter(), |res| err_count += res.is_err() as usize), 2);
+    assert_eq!(err_count, 0);
 }
